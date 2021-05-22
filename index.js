124a9f62787c8755c27f17e9dbbe7a2c8c5bc18f
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-22 20:20:01 -0300

    Primeros pasos servidor

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const conectarDB = require('./config/db')
+
+//crear el servidor
+
+const app = express();
+
+//Conectar a la base de datos
+
+conectarDB();
+
+//Habilitar express.json
+app.use(express.json({extended:true}));
+
+//puerto de la app
+const PORT = process.env.PORT || 4000;
+
+//Importar rutas
+app.use('/api/usuarios', require('./routes/usuarios'))
+app.use('/api/auth', require('./routes/auth'))
+
+//arrancar el servidor
+app.listen(PORT, ()=>{
+  console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+})
+
---
+++ routes/usuarios.js
@@ -0,0 +1,18 @@
+//Rutas para crear usuarios
+const express = require('express');
+const router = express.Router();
+const usuariosController = require('../controllers/usuarioController');
+const {check} = require('express-validator');
+
+//Crea un usuario
+//api/usuarios
+router.post('/', 
+[
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  check('email', 'Agrega un email valido').isEmail(),
+  check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6})
+],
+usuariosController.crearUsuario
+);
+
+module.exports = router;
