611bab89218562fc165bd88eae365712701b484a
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-22 22:19:57 -0300

    Adding GET y PUT on server

---
+++ routes/proyectos.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
+const {check} = require('express-validator');
+
+//Crea proyectos
+//api/proyectos
+router.post('/',
+  auth,
+  [
+    check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+  ],
+  proyectoController.crearProyectos
+)
+
+//Obtener todos los Proyectos
+router.get('/',
+  auth,
+  proyectoController.obtenerProyectos
+)
+
+//Actualizar proyecto via ID
+
+router.put('/:id', 
+auth,
+[
+  check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+],
+proyectoController.actualizarProyecto)
+
+module.exports = router;
