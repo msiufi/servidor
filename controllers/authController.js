5aa67843584e1e670e622604a03247349452c1d6
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-26 18:00:11 -0300

    user auth part 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -45,3 +45,15 @@ jwt.sign(payload, process.env.SECRETA, {
     console.log(error);
   }
 }
+
+//Obtiene que usuario esta autenticado
+
+exports.usuarioAutenticado = async (req, res) => {
+  try {
+    const usuario = await Usuario.findById(req.usuario.id);
+    res.json({usuario})
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({msg:'Hubo un error'})
+  }
+}
