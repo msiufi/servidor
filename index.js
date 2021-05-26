79828197080a782404cb4274649627a02a65fd35
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-26 17:44:35 -0300

    user auth

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const conectarDB = require('./config/db')
+const express = require("express");
+const conectarDB = require("./config/db");
+const cors = require("cors");
 
 //crear el servidor
 
@@ -9,20 +10,22 @@ const app = express();
 
 conectarDB();
 
+//Habilitar cors
+app.use(cors());
+
 //Habilitar express.json
-app.use(express.json({extended:true}));
+app.use(express.json({ extended: true }));
 
 //puerto de la app
 const PORT = process.env.PORT || 4000;
 
 //Importar rutas
-app.use('/api/usuarios', require('./routes/usuarios'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/proyectos', require('./routes/proyectos'))
-app.use('/api/tareas', require('./routes/tareas'))
+app.use("/api/usuarios", require("./routes/usuarios"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/proyectos", require("./routes/proyectos"));
+app.use("/api/tareas", require("./routes/tareas"));
 
 //arrancar el servidor
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`El servidor esta funcionando en el puerto ${PORT}`);
-})
-
+});
