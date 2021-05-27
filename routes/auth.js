91cf0dde6c94c7d39bc0d1db733a57919660ba73
Maximiliano Siufi <maxisiufi@gmail.com>
2021-05-27 17:54:28 -0300

    signIn auth

--- routes/auth.js
+++ routes/auth.js
@@ -8,10 +8,7 @@ const auth = require('../middleware/auth');
 //iniciar sesion
 //api/auth
 router.post('/', 
-[
-  check('email', 'Agrega un email valido').isEmail(),
-  check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6})
-],
+
 authController.autenticarUsuario
 );
 
