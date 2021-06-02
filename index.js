0076b5eb763d7d9fcf939d1118de10b54d5296ad
Maximiliano Siufi <maxisiufi@gmail.com>
2021-06-02 16:10:15 -0300

    Deployment fix

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Importar rutas
 app.use("/api/usuarios", require("./routes/usuarios"));
@@ -26,6 +26,6 @@ app.use("/api/proyectos", require("./routes/proyectos"));
 app.use("/api/tareas", require("./routes/tareas"));
 
 //arrancar el servidor
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`El servidor esta funcionando en el puerto ${PORT}`);
 });
